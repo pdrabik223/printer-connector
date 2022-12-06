d9d0aedc02f30340b4327785ed8c2e881520f761
pdrabik223 <pdrabik223@gmail.com>
2022-12-06 12:32:30 +0100

    formatted with black

--- examples/printer_connector_ci.py
+++ examples/printer_connector_ci.py
@@ -1,17 +1,17 @@
 from g_code_file_handler.g_code_file import GCodeFile
 from configs.exceptions import EndofFile
-from printer_connector.prusa_device import AnycubicDevice
+from printer_connector.anycubic_s_device import AnycubicDevice
 import time
 
 
 def print_from_file(printer: AnycubicDevice, file: GCodeFile) -> None:
     print("Commencing Printing.")
-    while True: 
+    while True:
         try:
             command = file.next_gcode_command()
 
             print(command)
-            printer.send((command))            
+            printer.send((command))
 
         except EndofFile as ex:
             print("Printing completed.")
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     printer = AnycubicDevice.connect_on_port("COM10", 115200)
 
     time.sleep(2)
-    print(printer.send('M115 U'))
-    print(printer.send('M115 U'))
-    print(printer.send('M115 U'))
-    print(printer.send('M105'))    
+    print(printer.send("M115 U"))
+    print(printer.send("M115 U"))
+    print(printer.send("M115 U"))
+    print(printer.send("M105"))
