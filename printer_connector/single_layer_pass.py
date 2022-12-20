612090c43247e4681cbc3fafd4867c65c554a12e
pdrabik223 <pdrabik223@gmail.com>
2022-12-20 16:01:28 +0100

    somethings wrong

--- printer_connector/single_layer_pass.py
+++ printer_connector/single_layer_pass.py
@@ -1,3 +1,4 @@
+import copy
 import math
 from typing import Tuple
 import numpy as np
@@ -10,7 +11,6 @@ def simple_pass(
     path_width: float,
 ):
     path = []
-
     y_direction = 1
 
     for x in range(0, math.floor((printer_size[0] - antenna_offset[0]) / path_width)):
@@ -43,9 +43,10 @@ def simple_pass(
 
 if __name__ == "__main__":
     #               x    y    z
-    printer_size = (200, 200, 200)
+    printer_size = (220, 220, 200)
     antenna_offset = (5, 10, 0)
-    path_width = 10
+    path_width = 5
+    antenna_measurement_radius = 30
     pass_height = 120
 
     path = simple_pass(
@@ -56,20 +57,36 @@ if __name__ == "__main__":
 
     path = [(pos[0], pos[1], pass_height) for pos in path]
 
-    path.insert(0, (0, 0, 0))
-
     x = [pos[0] for pos in path]
     y = [pos[1] for pos in path]
     z = [pos[2] for pos in path]
 
+    x_3d_plot = copy.deepcopy(x)
+    y_3d_plot = copy.deepcopy(y)
+    z_3d_plot = copy.deepcopy(z)
+
+    x_3d_plot.insert(0, 0)
+    y_3d_plot.insert(0, 0)
+    z_3d_plot.insert(0, 0)
+
     ax = plt.figure().add_subplot(projection="3d")
-    ax.plot(x, y, z)
-    ax.scatter(x, y, z, color="orange")
+    ax.plot(x_3d_plot, y_3d_plot, z_3d_plot)
+    ax.scatter(x_3d_plot, y_3d_plot, z_3d_plot, color="orange", alpha=0.4)
     ax.grid()
 
     ax = plt.figure().add_subplot()
     ax.plot(x, y)
-    ax.scatter(x, y, color="orange")
+    ax.scatter(x, y, color="orange", alpha=0.4)
+    ax.grid()
+
+    ax = plt.figure().add_subplot()
+    # ax.plot(x, y)
+    antenna_x = [pos[0] - antenna_offset[0] for pos in path]
+    antenna_y = [pos[1] - antenna_offset[1] for pos in path]
+    ax.scatter(x, y, color="orange", alpha=0.4)
+    ax.scatter(
+        antenna_x, antenna_y, color="green", alpha=0.4, s=antenna_measurement_radius
+    )
     ax.grid()
 
     plt.show()
