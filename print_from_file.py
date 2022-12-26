4176fa7c042ccce24c7003909eb4e6b7d93ac829
Piotr <piotr.drabik@pl.abb.com>
2022-12-26 23:27:27 +0100

    renamed file

---
+++ print_from_file.py
@@ -0,0 +1,28 @@
+from g_code_file_handler.g_code_file import GCodeFile
+from configs.exceptions import EndofFile
+from device_connector.marlin_device import MarlinDevice
+import time
+from serial import SerialException
+
+from device_connector.device import Device
+from device_connector.prusa_device import PrusaDevice
+
+
+def print_from_file(printer: Device, file: GCodeFile) -> None:
+    print("Commencing Printing.")
+    while True:
+        try:
+            command = file.next_gcode_command()
+            print(command)
+            # printer.send_and_await(command)
+
+        except EndofFile as ex:
+            print("Printing completed.")
+            return None
+
+
+if __name__ == "__main__":
+
+    printer = PrusaDevice.connect_on_port("COM8")
+    file = GCodeFile(file_name="C:\D\desk-spacer v6_0.15mm_PLA_MK3S_16m.gcode")
+    print_from_file(printer=printer, file=file)
