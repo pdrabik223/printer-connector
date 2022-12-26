ce1fc682f39159ad978189c4a9a26350df7ebfbe
Piotr <piotr.drabik@pl.abb.com>
2022-12-26 16:52:36 +0100

    working single layer pass

--- single_layer_pass.py
+++ single_layer_pass.py
@@ -1,8 +1,28 @@
 import copy
-import math
 from typing import List, Tuple
-import numpy as np
 import matplotlib.pyplot as plt
+
+
+def f_range(start: float = 0, end: float = 1, step: float = 1, include_start = True,include_end = False):
+    range = []
+
+    temp_value = start
+
+    if include_start:
+        range.append(temp_value)
+
+    temp_value += step
+    while temp_value < end:
+        range.append(temp_value)
+        temp_value += step
+
+    print(temp_value)
+
+    if include_end:
+        range.append(temp_value)
+
+    return range
+    
 
 
 def simple_pass(
@@ -10,50 +30,30 @@ def simple_pass(
     antenna_offset: Tuple[float, float, float],
     antenna_measurement_radius: float,
 ) -> List[Tuple[float, float]]:
+
+    # antenna diameter 
+    antenna_d = antenna_measurement_radius*2 
+    x_ps = printer_size[0]
+    y_ps = printer_size[1]
+
+    # list of extruder positions when measurement is done``
+    # not shifted by antenna offset
+    # measurements are spaced by antenna_measurement_radius
+    y_measurements_coords = [y for y in f_range(antenna_measurement_radius, y_ps, antenna_d, include_end=True)]
+    x_measurements_coords = [x for x in f_range(antenna_measurement_radius, x_ps, antenna_d, include_end=True)]
+
     path = []
-    y_direction = 1
+    for x in x_measurements_coords:
+        for y in y_measurements_coords:
+            path.append((x, y))
 
-    for x in range(
-        0,
-        math.floor((printer_size[0] - antenna_offset[0]) / antenna_measurement_radius),
-    ):
-        for y in range(
-            0,
-            math.floor(
-                (printer_size[1] - antenna_offset[1]) / antenna_measurement_radius
-            ),
-        ):
-            if y_direction == 1:
-                path.append(
-                    (
-                        x * antenna_measurement_radius,
-                        y * antenna_measurement_radius,
-                    )
-                )
-            else:
-                path.append(
-                    (
-                        x * antenna_measurement_radius,
-                        printer_size[1]
-                        - 2 * antenna_offset[1]
-                        - y * antenna_measurement_radius,
-                    )
-                )
-        y_direction *= -1
+    # shift by antenna offset
+    path = [(x + antenna_offset[0], y + antenna_offset[1]) for x, y in path]
 
-    # center on path
-    path = [
-        (
-            pos[0] + antenna_measurement_radius / 2,
-            pos[1] + antenna_measurement_radius / 2,
-        )
-        for pos in path
-    ]
+    # erase all positions that collide with boundaries of printer
+    path = [(x, y) for x, y in path if x <= x_ps and y <= y_ps]
 
-    # compensate for antenna to extruder offset
-    path = [(pos[0] + antenna_offset[0], pos[1] + antenna_offset[1]) for pos in path]
     return path
-
 
 def plot_measurement_areas(
     x_values: List[float],
@@ -67,10 +67,8 @@ def plot_measurement_areas(
         ax.add_patch(plt.Circle((x, y), radius, color=color, alpha=alpha))
 
 
-def create_3d_movement_plot(
-    path: List[Tuple[float, float]], pass_height: float
-):
-    
+def create_3d_movement_plot(path: List[Tuple[float, float]], pass_height: float):
+
     path = [(pos[0], pos[1], pass_height) for pos in path]
 
     x = [pos[0] for pos in path]
@@ -92,21 +90,37 @@ def create_3d_movement_plot(
     ax.axis("square")
     ax.grid()
 
-def create_2d_movement_plot(path: List[Tuple[float, float]]):
+
+def create_2d_movement_plot(
+    path: List[Tuple[float, float]], printer_boundaries: Tuple[float, float, float]
+):
+    ax = plt.figure().add_subplot()
+
+    x_printer_boundaries = (0, 0, printer_boundaries[0], printer_boundaries[0], 0)
+    y_printer_boundaries = (0, printer_boundaries[1], printer_boundaries[1], 1, 0)
+    ax.plot(x_printer_boundaries, y_printer_boundaries, color="black", alpha=0.9)
+
     x = [pos[0] for pos in path]
     y = [pos[1] for pos in path]
 
-    ax = plt.figure().add_subplot()
     ax.plot(x, y)
     ax.scatter(x, y, color="red")
+
     ax.grid()
-    
-def create_2d_measurement_placement_plot(path: List[Tuple[float, float]]):
+
+
+def create_2d_measurement_placement_plot(
+    path: List[Tuple[float, float]], printer_boundaries: Tuple[float, float, float]
+):
+    ax = plt.figure().add_subplot()
+
+    x_printer_boundaries = (0, 0, printer_boundaries[0], printer_boundaries[0], 0)
+    y_printer_boundaries = (0, printer_boundaries[1], printer_boundaries[1], 1, 0)
+    ax.plot(x_printer_boundaries, y_printer_boundaries, color="black", alpha=0.9)
+
     x = [pos[0] for pos in path]
     y = [pos[1] for pos in path]
-    
-    ax = plt.figure().add_subplot()
-    
+
     antenna_x = [pos[0] - antenna_offset[0] for pos in path]
     antenna_y = [pos[1] - antenna_offset[1] for pos in path]
 
@@ -115,11 +129,12 @@ def create_2d_measurement_placement_plot(path: List[Tuple[float, float]]):
     ax.axis("square")
     ax.grid()
 
+
 if __name__ == "__main__":
     #               x    y    z
-    printer_size = (211.5, 211.5, 200)
+    printer_size = (230, 230, 200)
     antenna_offset = (5, 10, 0)
-    antenna_measurement_radius = 30
+    antenna_measurement_radius = 10
     pass_height = 120
 
     path = simple_pass(
@@ -128,7 +143,8 @@ if __name__ == "__main__":
         antenna_offset=antenna_offset,
     )
     create_3d_movement_plot(path=path, pass_height=pass_height)
-    create_2d_movement_plot(path=path)
-    create_2d_measurement_placement_plot(path=path)
-
+    create_2d_movement_plot(path=path, printer_boundaries=printer_size)
+    create_2d_measurement_placement_plot(path=path, printer_boundaries=printer_size)
     plt.show()
+
+    
