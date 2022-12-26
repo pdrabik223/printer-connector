0d7f289db6d151baa1f03d12114bbaa489fe6715
Piotr <piotr.drabik@pl.abb.com>
2022-12-26 16:54:38 +0100

    deleting temp file

--- temp.py
+++
@@ -1,8 +0,0 @@
-from printer_connector.anycubic_s_device import AnycubicSDevice
-
-if __name__ == "__main__":
-    examples = ("0", "1", "2", "G1", "N1G1N1")
-    for e in examples:
-        print(e)
-        print(AnycubicSDevice.checksum(e))
-
