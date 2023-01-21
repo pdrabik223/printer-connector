8bff6a137eb4f26e3399f0090929197eb7037a97
pdrabik223 <pdrabik223@gmail.com>
2023-01-21 14:53:57 +0100

    correctly oriented plots

--- gui_plots/gui_plots.py
+++ gui_plots/gui_plots.py
@@ -26,12 +26,12 @@ class PathPlotCanvas(FigureCanvas):
         super(PathPlotCanvas, self).__init__(self.fig)
 
     def plot_data(
-        self,
-        path: List[Point],
-        printer_boundaries: Tuple[float, float, float],
-        antenna_offset: Tuple[float, float, float],
-        antenna_measurement_radius: float,
-        highlight: Optional[Point] = None,
+            self,
+            path: List[Point],
+            printer_boundaries: Tuple[float, float, float],
+            antenna_offset: Tuple[float, float, float],
+            antenna_measurement_radius: float,
+            highlight: Optional[Point] = None,
     ):
         # x = [pos[0] for pos in path]
         # y = [pos[1] for pos in path]
@@ -78,71 +78,18 @@ class PathPlotCanvas(FigureCanvas):
 
     @staticmethod
     def plot_measurement_areas(
-        x_values: List[float],
-        y_values: List[float],
-        ax: plt.Axes,
-        radius: float,
-        color: str = "orange",
-        alpha: float = 0.2,
+            x_values: List[float],
+            y_values: List[float],
+            ax: plt.Axes,
+            radius: float,
+            color: str = "orange",
+            alpha: float = 0.2,
     ) -> None:
         for x, y in zip(x_values, y_values):
             ax.add_patch(plt.Circle((x, y), radius, color=color, alpha=alpha))
 
     def show(self):
         self.fig.draw()
-
-
-#
-# class Path2DPlotCanvas(FigureCanvas):
-#     def __init__(self, parent=None, width=9, height=5, dpi=90):
-#         self.fig = Figure(figsize=(width, height), dpi=dpi)
-#
-#         self.axes = self.fig.add_subplot(111)
-#
-#         self.fig.tight_layout()
-#         super(Path2DPlotCanvas, self).__init__(self.fig)
-#
-#     @staticmethod
-#     def plot_measurement_areas(
-#             x_values: List[float],
-#             y_values: List[float],
-#             ax: plt.Axes,
-#             radius: float,
-#             color: str = "orange",
-#             alpha: float = 0.2,
-#     ) -> None:
-#         for x, y in zip(x_values, y_values):
-#             ax.add_patch(plt.Circle((x, y), radius, color=color, alpha=alpha))
-#
-#     def plot_data(self, path: List[Point], printer_boundaries: Tuple[float, float, float],
-#                   antenna_offset: Tuple[float, float, float], antenna_measurement_radius: float,
-#                   highlight: Optional[Point] = None):
-#         x_printer_boundaries = (0, 0, printer_boundaries[0], printer_boundaries[0], 0)
-#         y_printer_boundaries = (0, printer_boundaries[1], printer_boundaries[1], 0, 0)
-#
-#         x = [pos[0] for pos in path]
-#         y = [pos[1] for pos in path]
-#
-#         antenna_x = [pos[0] - antenna_offset[0] for pos in path]
-#         antenna_y = [pos[1] - antenna_offset[1] for pos in path]
-#
-#         self.axes.cla()
-#         Path2DPlotCanvas.plot_measurement_areas(antenna_x, antenna_y, self.axes, antenna_measurement_radius)
-#         self.axes.plot(x_printer_boundaries, y_printer_boundaries, color="black", alpha=0.6)
-#         self.axes.plot(x, y)
-#         self.axes.scatter(x, y, color="red")
-#
-#         if highlight is not None:
-#             self.axes.add_patch(plt.Circle((highlight[0], highlight[1]), 2, color="black", alpha=1))
-#
-#         self.axes.axis("square")
-#         self.axes.grid()
-#         self.axes.set_xlabel("X [arb. units]")
-#         self.axes.set_ylabel("Y [arb. units]")
-#         self.axes.set_title("Some thing")
-#
-#     def show(self):
-#         self.fig.draw()
 
 
 class MeasurementsPlotCanvas(FigureCanvas):
@@ -152,14 +99,19 @@ class MeasurementsPlotCanvas(FigureCanvas):
         self.axes = self.fig.add_subplot(111)
 
         self.fig.tight_layout()
+
+        self.cp = None
+        self.cbar = self.fig.colorbar(self.cp, ax=self.axes, extend='both')
+        self.cbar.minorticks_on()
+
         super(MeasurementsPlotCanvas, self).__init__(self.fig)
 
     def plot_data(
-        self,
-        path: List[Tuple[float, float, float]],
-        measurements: List[Tuple[float, float, float, float]],
+            self,
+            path: List[Tuple[float, float, float]],
+            measurements: List[Tuple[float, float, float, float]],
     ):
-
+        self.axes.cla()
         no_bins_x = np.unique([x for x, _, _ in path])
         no_bins_y = np.unique([y for _, y, _ in path])
 
@@ -176,14 +128,21 @@ class MeasurementsPlotCanvas(FigureCanvas):
                 val[-1].append(filler)
 
         k: int = 0
-        for y in range(len(no_bins_y)):
-            for x in range(len(no_bins_x)):
+        for x in range(len(no_bins_x)):
+            for y in range(len(no_bins_y) - 1, -1, -1):
                 if k >= len(values):
                     break
                 val[y][x] = values[k]
                 k += 1
 
-        cp = self.axes.imshow(val)
+        self.cp = self.axes.imshow(val, cmap='RdBu', vmin=np.min(val), vmax=np.max(val),
+                                   interpolation='none')
+
+        # self.cbar = self.fig.colorbar(self.cp, ax=self.axes, extend='both')
+        # self.cbar.ax = self.axes
+        # self.cbar.extend = 'both'
+        self.cbar.mappable = self.cp
+
         self.axes.set_xlabel("X [arb. units]")
         self.axes.set_ylabel("Y [arb. units]")
         self.axes.set_title("Some thing")
